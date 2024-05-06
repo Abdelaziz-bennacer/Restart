02c777f2810bba0fceba4648f60b77c5a754b534
Abdelaziz Bennacer <a.bennacer@compethance.fr>
2024-05-06 11:15:50 +0200

    Initial Commit

---
+++ Restart/RestartApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartApp.swift
+//  Restart
+//
+//  Created by Abdelaziz Bennacer on 06/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct RestartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
